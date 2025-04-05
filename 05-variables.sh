af6f3f893559fb278da411781ab4fccbb933c09c
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-04-06 00:26:50 +0530

    shel-script

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,7 @@
 
 echo "Please enter your username::" # text entered here will be added as value to variable
 
-read -s USERNAME # Here USERNAME is the variable name
+read  USERNAME # Here USERNAME is the variable name
 
 echo "username entered: $USERNAME"
 
