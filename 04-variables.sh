75bea05a817300acab2e6d8c8c01fe32f31034aa
juturuajay <86615742+juturuajay@users.noreply.github.com>
2025-04-06 00:12:27 +0530

    shel-script

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PERSON1=RAHIM   #no space btw before and after equal
-PERSON2=ROBERT 
+PERSON1=$1  #no space btw before and after equal
+PERSON2=$2 
 
 echo "$PERSON1: Hi $PERSON2"  #Use $ or ${var-name} to access the value of a variable
 echo "${PERSON2}: Hello $PERSON1"
 echo "$PERSON1: How are you doing"
 echo "$PERSON2: I'm doing well, how about you"
+
